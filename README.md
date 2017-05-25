7e427e3c866be51b834cc9cfd823de6d1c445ea4
PrL327 <peter.laskai@gmail.com>
2017-05-25 16:13:43 -0400

    5/25 Added Screenshot functionality

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ mvn clean install -Dmaven.test.skip=true
 ## Run test
 mvn test
 
-### TEST COMMIT
+### TEST COMMIT -5/18
 Peter's Tests w/branches
+
+### 5/25/2017
+Added Screenshot functionality:
+   Program takes a screencap everytime there is a test failure and stores it in a folder
--- src/test/java/Screenshot.java
+++ src/test/java/Screenshot.java
@@ -1,7 +1,63 @@
-package PACKAGE_NAME;
-
 /**
  * Created by plaskai on 5/25/17.
  */
-public class Screenshot {
-}
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+
+    public class Screenshot {
+        WebDriver driver;
+
+        @Before
+        public void setUp() throws Exception {
+            System.setProperty("webdriver.chrome.driver", "./src/test/java/chromedriverMAC");
+            driver = new ChromeDriver();
+        }
+        @Rule
+        public TestRule watcher = new TestWatcher() {
+            @Override
+            protected void failed(Throwable throwable, Description description) {
+                File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+                try {
+                    FileUtils.copyFile(scrFile,
+                            new File("failshot_"
+                                    + description.getClassName()
+                                    + "_" + description.getMethodName()
+                                    + ".png"));
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            @Override
+            protected void finished(Description description) {
+                driver.quit();
+            }
+        };
+        @Test
+        public void OnError() {
+            driver.get("http://google.com");
+            assertTrue(true);
+            WebElement element = driver.findElement(By.id("lst-ib"));
+            element.clear();
+            element.sendKeys("mother's day");
+            driver.findElement(By.cssSelector(".sbico svg")).click();
+            element.sendKeys(Keys.RETURN);
+            assertEquals("Mother's Day 2017 in United States of America1", driver.findElement(By.cssSelector(".vk_gy.vk_sh.whenis span:nth-of-type(2)")).getText());
+        }
+
+
+
+
+    }
