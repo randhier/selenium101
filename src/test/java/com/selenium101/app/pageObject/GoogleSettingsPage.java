0464c18a74b03d0e03d4db7a2cc3cb5fd37ca2db
PrL327 <peter.laskai@gmail.com>
2017-07-14 22:05:01 -0400

    created GoogleSettingsPage Object

---
+++ src/test/java/com/selenium101/app/pageObject/GoogleSettingsPage.java
@@ -0,0 +1,13 @@
+package com.selenium101.app.pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by plaskai on 7/14/17.
+ */
+public class GoogleSettingsPage extends BasePage {
+
+    public GoogleSettingsPage(WebDriver driver){
+        super(driver);
+    }
+}
